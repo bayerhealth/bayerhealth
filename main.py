d820351704c0ab706e146fc9b478c5e72e568dc0
Kajetan Knopp <kknopp@staszic.waw.pl>
2021-05-29 00:30:43 +0200

    Ready for plants

--- main.py
+++ main.py
@@ -5,10 +5,22 @@ import sys
 import json
 import string
 from flask import Flask, flash, render_template, redirect, request, url_for, jsonify, session, Response
+from utils import handle_video
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = 'static/plants'
+ALLOWED_EXTENSIONS = {'jpg'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 
 app = Flask(__name__)
 app.secret_key = 'trzyq'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def main():
@@ -16,8 +28,26 @@ def main():
 
 
 @app.route('/index', methods=["GET", 'POST'])
-def upload():
-    return render_template('index.html')
+def index():
+    if request.method == "GET":
+        return render_template('index.html')
+    elif request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            rand_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+            filename = secure_filename(rand_id + '.jpg')
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            handle_video(rand_id)
+            return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,3 @@
+def handle_video(video_id):
+    filepath = 'static/vid/'+video_id+'.mp4'
+    print("Uploaded: " + filepath)
