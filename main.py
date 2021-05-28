da206e5d2079dd81a914e22c9a56046b51a23884
Kajetan Knopp <kknopp@staszic.waw.pl>
2021-05-28 22:04:12 +0200

    fixed flask

--- main.py
+++ main.py
@@ -6,54 +6,18 @@ import json
 import string
 from flask import Flask, flash, render_template, redirect, request, url_for, jsonify, session, Response
 
-from werkzeug.utils import secure_filename
-
-
-UPLOAD_FOLDER = 'static/vid'
-ALLOWED_EXTENSIONS = {'mp4'}
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
 
 app = Flask(__name__)
-app.secret_key = '9je0jaj09jk9dkakdwjnjq'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'trzyq'
 
 @app.route('/')
 def main():
-    if 'username' in session:
-        return redirect(url_for('upload'))
-    else:
-        return redirect(url_for('viewall'))
+    return redirect(url_for('index'))
 
 
-@app.route('/upload', methods=["GET", 'POST'])
+@app.route('/index', methods=["GET", 'POST'])
 def upload():
-    if request.method == "GET":
-        if 'username' in session:
-            return render_template('upload.html', username=session.get('username'))
-        else:
-            return redirect(url_for('login'))
-    elif request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file:
-            rand_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-            filename = secure_filename(rand_id + '.mp4')
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            handle_video(rand_id)
-            return redirect(url_for('viewall'))
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
